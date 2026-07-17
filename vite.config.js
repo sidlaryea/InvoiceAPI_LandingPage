58d8197aeaa5ae2b286538ccb7881673b409ade2
Sidney Laryea <slaryea@ghanaports.gov.gh>
2026-07-17 11:48:20 +0000

    update proxy target in vite.config.js to use direct URL

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import {API_BASE} from "../InvoiceLandingPage/src/config/api"
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/Profile-images': {
-        target: `${API_BASE}`,
+        target: `https://invoiceapi-gcc3duhbc4age6bw.southafricanorth-01.azurewebsites.net`,
         changeOrigin: true,
         secure: false,
       },
